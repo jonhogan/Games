d8efd2c2edd1a88c155ae8b665b33191f66aabc8
Jon <jon.hogan83@gmail.com>
2022-06-15 23:11:32 -0500

    Finish Enums

--- C_Sharp/enums/Program.cs
+++ C_Sharp/enums/Program.cs
@@ -7,9 +7,21 @@ while(true){
     Console.WriteLine("What would you like to do to the box? (Open, Close, lock, or Unlock)");
     newState = Console.ReadLine();
 
-    if(State == 0 && (newState == "Close" || newState == "close")){
+    if(State == Box.Open && (newState == "Close" || newState == "close")){
         Console.WriteLine("You closed the box.");
-    }else if(State == 1 && (new))
+        State = Box.Closed;
+    }else if(State == Box.Closed && (newState == "Open" || newState == "open")){
+        Console.WriteLine("You open the box");
+        State = Box.Open;
+    }else if(State == Box.Closed && (newState == "Lock" || newState == "lock")){
+        Console.WriteLine("You lock the box.");
+        State = Box.Locked;
+    }else if(State == Box.Locked && (newState == "Unlock" || newState == "unlock")){
+        Console.WriteLine("You unlock the box.");
+        State = Box.Closed;
+    }else{
+        Console.WriteLine("You can't do that. If the box is open, you can close it. If it is closed, you can open or lock it. If it is locked, you can unlock it.");
+    }
 }
 
 enum Box{
