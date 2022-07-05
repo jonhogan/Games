94cfc08a0e1486ff53527d77313dbda1c8e77962
Jon <jon.hogan83@gmail.com>
2022-07-05 12:15:22 -0500

    Java Hello World

---
+++ java_lang/hello_world/hello.java
@@ -0,0 +1,13 @@
+/*
+ * We must do a "Hello world"!
+ * You can't say you are learning a language with out
+ * doing "Hello World".
+ */
+
+public class hello{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello you wild and crazy world!");
+    }
+}
