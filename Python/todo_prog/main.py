c00af58267a891ccb59dcf0f9fcafb3f83eba2cf
Jon Hogan <jon.hogan83@gmail.com>
2023-06-05 21:58:07 -0500

    Updated ToDo list program

--- Python/todo_prog/main.py
+++ Python/todo_prog/main.py
@@ -7,9 +7,28 @@ def get_todo():
     todo = input('Enter a todo: ')
     return todo
 
-todo_list = []
-for i in range(3):
-    todo_list.append(get_todo())
+def get_continue():
+    while True:
+        choice = input("Do you have more items (yes/no)? ")
 
+        if choice.lower() in ('n', 'no'):
+            return False
+        elif choice.lower() in ('y', 'yes'):
+            return True
+        else:
+            print('Invalid input')
+
+def make_list():
+    todo_list = []
+
+    choice = True
+    while choice:
+        todo_list.append(get_todo())
+        choice = get_continue()
+
+    return todo_list
+
+
+todo_list = make_list() 
 for todo in todo_list:
     print(todo)
