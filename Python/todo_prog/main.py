fd66b5c1a95d0d96182baa44819863eed35d0378
Jon Hogan <jon.hogan83@gmail.com>
2023-06-11 14:46:54 -0500

    Added "complete" function

--- Python/todo_prog/main.py
+++ Python/todo_prog/main.py
@@ -2,6 +2,7 @@
 This is a ToDo list created from the Udemy course:
     The Python Mega Course: Learn Python in 60 Days with 20 Apps
 """
+'''TODO created a main function, set the program to call the main function'''
 
 def get_todo():
     todo = input('Enter a todo: ')
@@ -20,8 +21,19 @@ def get_continue():
 
 
 def print_menu():
-        options = input("Would you like to ADD, EDIT, SHOW, or EXIT? ").lower().strip()
-        return options.lower().strip()
+        '''TODO add a list of options, ask user to make a selection
+           print choices and pass the option back to the main function'''
+        print("Would you like to: ADD, EDIT, SHOW, or EXIT? ")
+        selections = ["ADD", "EDIT", "SHOW", "COMPLETE", "EXIT"]
+        for i, selection in enumerate(selections, 1):
+            print(f" {i} : {selection}")
+        
+        option = input("Enter option here: ")
+
+        if option.isdigit():
+            return int(option)
+        else:
+            return option.lower().strip()
 
 def make_list():
     todo_list = []
@@ -32,6 +44,18 @@ def make_list():
         add_more = get_continue()
 
     return todo_list
+def complete_item(todo_list):
+    print_todo(todo_list)
+    while True: 
+        selection = input("Enter item number to complete: ")
+        if selection.isdigit() and (int(selection) <= len(todo_list)):
+            selection = int(selection) - 1
+            todo_list.remove(todo_list[selection])
+
+            return todo_list
+
+        else:
+            print("Invalid selection, please try again.")
 
 def print_todo(todo_list):
     for i, todo in enumerate(todo_list, 1):
@@ -60,13 +84,15 @@ todo_list = []
 while True:
     options = print_menu()
     match options:
-        case 'add':
+        case 'add' | 1:
             todo_list = make_list()
-        case 'show':
+        case 'show' | 3:
                 print_todo(todo_list)
-        case 'edit':
+        case 'edit' | 2:
             todo_list = get_edit_choice(todo_list)
-        case 'exit':
+        case 'complete' | 4:
+            todo_list = complete_item(todo_list)
+        case 'exit' | 5:
             break
         case other:
             print('Invalid option. Please enter "add", "show", "edit", or "exit"')
