f0502a90b4cc49e21bd728e25dd33535fc01342b
Jon Hogan <jon.hogan83@gmail.com>
2023-06-13 12:15:56 -0500

    updated main

--- Python/todo_prog/main.py
+++ Python/todo_prog/main.py
@@ -1,97 +1,95 @@
-"""
-This is a ToDo list created from the Udemy course:
-    The Python Mega Course: Learn Python in 60 Days with 20 Apps
-"""
-'''TODO created a main function, set the program to call the main function'''
+"""Updated Todo program"""
 
-def get_todo():
+
+def add_todo():
     todo = input('Enter a todo: ')
     return todo
 
-def get_continue():
-    while True:
-        choice = input("Do you have more items (yes/no)? ")
+def read_file():
+    """Reads todo.txt file and returns list of to-dos"""
+    todo_list = []
+    with open('todo.txt', 'r') as file:
+        for line in file:
+            todo_list.append(line.strip())
+    return todo_list
 
-        if choice.lower() in ('n', 'no'):
-            return False
-        elif choice.lower() in ('y', 'yes'):
-            return True
-        else:
-            print('Invalid input')
-
+def write_file(todo_list):
+    """Writes todo_list to todo.txt"""
+    with open('todo.txt', 'w') as file:
+        for todo in todo_list:
+            this_todo = todo + '\n'
+            file.write(this_todo)
 
 def print_menu():
-    print("Would you like to: ADD, EDIT, SHOW, or EXIT? ")
+    """Prints menu and returns choice"""
     selections = ["ADD", "EDIT", "SHOW", "COMPLETE", "EXIT"]
     for i, selection in enumerate(selections, 1):
         print(f" {i} : {selection}")
-        
+
     option = input("Enter option here: ")
 
     if option.isdigit():
         return int(option)
     else:
         return option.lower().strip()
-
-def make_list():
-    todo_list = []
-    add_more = True
-
-    while add_more:
-        todo_list.append(input("What is your to do item? ") + "\n")
-        add_more = get_continue()
-    file = open('todo.txt', 'w')
-    file.writelines(todo_list)
-    return todo_list
-def complete_item(todo_list):
-    print_todo(todo_list)
-    while True: 
-        selection = input("Enter item number to complete: ")
-        if selection.isdigit() and (int(selection) <= len(todo_list)):
-            selection = int(selection) - 1
-            todo_list.remove(todo_list[selection])
-
-            return todo_list
-
-        else:
-            print("Invalid selection, please try again.")
-
-def print_todo(todo_list):
+    
+def print_todo_list(todo_list):
+    """Prints todo_list"""
     for i, todo in enumerate(todo_list, 1):
         print( f"{i}: {todo}")
 
-def edit_list(todo_list, element):
-    todo_list[element - 1] = input("What is the new To Do item? ")
-    return todo_list
+def edit_list(todo_list):
+    """Edit or delete items in todo_list"""
+    print_todo_list(todo_list)
 
-def get_edit_choice(todo_list):
-    print_todo(todo_list)
-    
-    while True: 
+    while True:
         selection = input("Enter item number to edit: ")
         if selection.isdigit() and (int(selection) <= len(todo_list)):
             selection = int(selection)
-            todo_list = edit_list(todo_list, selection)
-
+            todo_list[selection - 1] = input("What is the new To Do item? ")
             return todo_list
 
         else:
             print("Invalid selection, please try again.")
-        
 
-todo_list = []
-while True:
-    options = print_menu()
-    match options:
-        case 'add' | 1:
-            todo_list = make_list()
-        case 'show' | 3:
-                print_todo(todo_list)
-        case 'edit' | 2:
-            todo_list = get_edit_choice(todo_list)
-        case 'complete' | 4:
-            todo_list = complete_item(todo_list)
-        case 'exit' | 5:
-            break
-        case other:
-            print('Invalid option. Please enter "add", "show", "edit", or "exit"')
+def delete_item(todo_list):
+    """Delete item from todo_list"""
+    print_todo_list(todo_list)
+
+    while True:
+        selection = input("Enter item number to delete/complete: ")
+        if selection.isdigit() and (int(selection) <= len(todo_list)):
+            selection = int(selection)
+            todo_list.remove(todo_list[selection - 1])
+            return todo_list
+
+        else:
+            print("Invalid selection, please try again.")
+
+def save_and_exit(todo_list):
+    """Save todo_list to todo.txt and exit program"""
+    write_file(todo_list)
+    print("Your to-do list has been saved.")
+    exit()
+
+def main():
+    todo_list = read_file()
+    while True:
+        options = print_menu()
+        match options:
+            case 1 | 'add' | 'a':
+                todo = add_todo()
+                todo_list.append(todo)
+            case 2 | 'edit' | 'e':
+                todo_list = edit_list(todo_list)
+            case 3 | 'show' | 's':
+                print_todo_list(todo_list)
+            case 4 | 'complete' | 'c':
+                todo_list = delete_item(todo_list)
+            case 5 | 'exit' | 'x':
+                save_and_exit(todo_list)
+            case _:
+                print("Invalid selection, please try again.")
+
+if __name__ == '__main__':
+    main()
