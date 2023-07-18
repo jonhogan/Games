12ef8689d0a3d7eb646c469ed3b4569bec0b26d8
Jon Hogan <jon.hogan83@gmail.com>
2023-07-18 13:51:12 -0500

    Try/Except data validation

--- Python/atbs/section_4/try_except/main.py
+++ Python/atbs/section_4/try_except/main.py
@@ -4,3 +4,23 @@ def div42(x):
         print(42 / x)
     except ZeroDivisionError:
         print("Error: You tried to divide by Zero")
+
+
+div42(3)
+div42(0)
+
+
+'''
+Try and Except can also be used for data validation
+'''
+
+while True:
+    try:
+        x = int(input('Enter a number: '))
+        break
+    except:
+        print("That is not a number")
+
+'''
+Will not break out of the while loop until the a number is entered
+'''
