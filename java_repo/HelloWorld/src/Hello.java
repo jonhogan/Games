ee14a37181e1c1736de5f6bcd265260f13d43517
Jon <jon.hogan83@gmail.com>
2023-07-27 22:43:25 -0500

    Initial Push

--- java_repo/HelloWorld/src/Hello.java
+++ java_repo/HelloWorld/src/Hello.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jon");
+
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("Is not an alien!");
+            System.out.println("And I'm scare of aliens");
+        }
+    }
 }
