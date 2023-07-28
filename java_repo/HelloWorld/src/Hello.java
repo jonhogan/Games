2d3f64b3c9f458871cf9e583d8642c48eee51a47
Jon <jon.hogan83@gmail.com>
2023-07-27 23:30:46 -0500

    If statements

--- java_repo/HelloWorld/src/Hello.java
+++ java_repo/HelloWorld/src/Hello.java
@@ -1,4 +1,5 @@
 public class Hello {
+
     public static void main(String[] args) {
         System.out.println("Hello, Jon");
 
@@ -7,5 +8,20 @@ public class Hello {
             System.out.println("Is not an alien!");
             System.out.println("And I'm scare of aliens");
         }
+
+        int topScore = 90;
+        if (topScore >= 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if(topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
     }
 }
