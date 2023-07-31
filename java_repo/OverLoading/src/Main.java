427b940d5fde8ea6af65ffb8ae3d3e1454779293
Jon Hogan <jon.hogan83@gmail.com>
2023-07-30 19:11:42 -0500

    Initial puush

--- java_repo/OverLoading/src/Main.java
+++ java_repo/OverLoading/src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        int newScore = calculateScore("Jon", 1337);
+
+        System.out.println("New score is " + newScore + " points");
+
+        newScore = calculateScore(241);
+
+        System.out.println("New score is " + newScore + " points");
+
+    }
+
+    public static int calculateScore(String playerName, int score){
+
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score *1_000;
+    }
+
+    public static int calculateScore(int score){
+
+        System.out.println("Player scored " + score + " points");
+        return score *1_000;
+    }
 }
