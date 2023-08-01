5e59777c7bba47f472bd714c5874ac1a53f213d8
Jon Hogan <jon.hogan83@gmail.com>
2023-08-01 15:40:58 -0500

    Updated switch statements to switch expressions

--- java_repo/Switch/src/Main.java
+++ java_repo/Switch/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        switch (1) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            default -> System.out.println("Was not 1 or 2");
+        }
+
+        /*
+
+         The above as an if statement
+
         int value = 1;
 
         if (value == 1){
@@ -10,5 +20,22 @@ public class Main {
         } else{
             System.out.println("Was not 1 or 2");
         }
+
+         */
+
+        String month = "MARCH";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    @org.jetbrains.annotations.Contract(pure = true)
+    public static String getQuarter(String month){
+
+        return switch (month){
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
     }
 }
