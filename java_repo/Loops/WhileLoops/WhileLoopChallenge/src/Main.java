a997cd7d083c758c1db1e7e987500fa04d862de8
Jon Hogan <jon.hogan83@gmail.com>
2023-08-08 19:31:01 -0500

    Added counter for total numbers

--- java_repo/Loops/WhileLoops/WhileLoopChallenge/src/Main.java
+++ java_repo/Loops/WhileLoops/WhileLoopChallenge/src/Main.java
@@ -3,21 +3,26 @@ public class Main {
         int number = 4;
         int finishNumber = 20;
         int counter = 0;
+        int totalNumbers = 0;
 
         while (number <= finishNumber){
             number++;
             if(!isEven(number)){
+                totalNumbers++;
                 continue;
             }
             if(counter == 5){
                 break;
             }
             System.out.println("Even Number: " + number);
+            totalNumbers++;
             counter++;
         }
 
         System.out.println("A total of " + counter +
                 " even numbers were found.");
+        System.out.println("A total of " + totalNumbers +
+                " numbers were processed.");
     }
 
     public static boolean isEven(int x){
