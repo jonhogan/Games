955e1a6be8086e9c39bdcad39d982f5adcbd489d
Jon Hogan <jon.hogan83@gmail.com>
2023-08-15 14:00:00 -0500

    Moved folders

---
+++ java_repo/Scanner/InputChallenge/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int count = 0;
+        int sum = 0;
+        String uerInput = "";
+
+        while (true){
+            sum += validateInteger();
+
+        }
+
+
+    }
+
+    public static int validateInteger(){
+        return 1;
+    }
+
+    public static String getInput(){
+        return "";
+    }
+}
