c36e6b976568da7615279593f359db1c27a900d4
Jon Hogan <jon.hogan83@gmail.com>
2023-08-14 21:33:50 -0500

    Push

--- java_repo/Scanner/UserInput/src/Main.java
+++ java_repo/Scanner/UserInput/src/Main.java
@@ -1,2 +1,80 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        int currentYear = 2023;
+
+        /*
+        String yearOfBirth = "1983";
+
+        int intYearOfBirth = Integer.parseInt(yearOfBirth);
+
+        System.out.println("Your age is: " + (currentYear - intYearOfBirth));
+
+         */
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        }catch (NullPointerException e){
+            System.out.println(getInputFromScanner(currentYear));
+        }
+
+    }
+
+    public static String getInputFromConsole(int currentYear){
+
+        String name = System.console().readLine("Hi, what's your name? ");
+        System.out.println("Hi, " + name + ", it is nice to meet you.");
+        String yearOfBirth = System.console().readLine("What year were you born? " );
+
+        int age = currentYear - (Integer.parseInt(yearOfBirth));
+
+        return "You are " + age + " years old.";
+    }
+
+    public static String getInputFromScanner (int currentYear){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, what is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + ", it is nice to meet you.");
+
+        int age = checkData(currentYear);
+        return "You are " + age + " years old.";
+    }
+
+    public static int checkData(int currentYear){
+        Scanner scanner = new Scanner(System.in);
+        String birthYear = "";
+        int yearOfBirth = 0;
+
+        boolean isNumber = false;
+
+        while(!isNumber){
+            System.out.println("What year were you born? ");
+            birthYear = scanner.nextLine();
+            try {
+                yearOfBirth = Integer.parseInt(birthYear);
+
+            }catch(NumberFormatException n) {
+                System.out.println("Please enter a valid numeric year.");
+                continue;
+            }
+
+            if(yearOfBirth < 0){
+                System.out.println("Year of birth must be a positive number.");
+            }else if ((currentYear - yearOfBirth) > 125){
+                System.out.println("It is highly unlikely you are 125 or greater years old,"+
+                        " please re-enter your birth year");
+            }else{
+                isNumber = true;
+            }
+        }
+
+        return currentYear - yearOfBirth;
+
+    }
 }
