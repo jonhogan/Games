0fb0ecd5b5a2b9d17404ed05283c777802050fab
Jon <jon.hogan83@gmail.com>
2023-08-15 22:14:45 -0500

    Initial Push

---
+++ java_repo/Scanner/MinMax/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Set an arbitrary large number for min
+        // and an arbitrary small number for max
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            try{
+                
+            }
+        }
+    }
+}
