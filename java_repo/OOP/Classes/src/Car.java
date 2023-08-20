ada45edbbbe7d28aa403aff45288a73484080040
Jon Hogan <jon.hogan83@gmail.com>
2023-08-20 09:35:04 -0500

    Initial Push

---
+++ java_repo/OOP/Classes/src/Car.java
@@ -0,0 +1,37 @@
+public class Car {
+
+    private String make;
+    private String model;
+    private String color;
+    private int numberOfDoors;
+    private boolean convertible;
+
+    public void describeCar(){
+        System.out.println("Doors: " + numberOfDoors + "-Door\n" +
+                "Color: " + color + "\n" +
+                "Make: " + make + "\n" +
+                "Model: " + model + "\n" +
+                "Top: " + (convertible ? "Convertible" : "Hard Top"));
+    }
+
+    public void setMake(String thisMake){
+        make = thisMake;
+    }
+
+    public void setModel(String thisModel){
+        model = thisModel;
+    }
+
+    public void setNumberOfDoors(int doors){
+        numberOfDoors = doors;
+    }
+
+    public void setColor(String thisColor){
+        color = thisColor;
+    }
+
+    public void setConvertible(boolean isConvertible){
+        convertible = isConvertible;
+    }
+
+}
